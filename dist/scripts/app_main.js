db715f91321bb7a590963c3677b7a11ca76ff261
Graham Perry <graham.perry@toronto.ca>
2017-10-25 10:07:34 -0400

    Add auth to config.httpHost
    		"auth": {
    			"local": "https://was-intra-sit.toronto.ca",
    			"dev": "https://was-intra-sit.toronto.ca",
    			"qa": "https://insideto-secure.toronto.ca",
    			"prod": "https://insideto-secure.toronto.ca"
    		},
    
    Updated init method oLogin (cot_login).

--- dist/scripts/app_main.js
+++ dist/scripts/app_main.js
@@ -75,7 +75,7 @@ function init() {
     crossroads.addRoute('{formName}/new/:?query:', newPage);
     crossroads.addRoute('{formName}/{id}/:?query:', viewEditPage);
     oLogin = new cot_login({
-        ccRoot: config.httpHost.app[httpHost],
+        ccRoot: config.httpHost.auth[httpHost],
         ccPath: config.api.authPath,
         ccEndpoint:config.api.authEndpoint,
         welcomeSelector: "#app-content-right",
