7de75ba3654750833a98a08ed580aa954a6e617d
Graham Perry <graham.perry@toronto.ca>
2017-11-07 09:29:58 -0500

    Small change to openView method.
    Added export to csv method for evaluation.

--- dist/scripts/app_main.js
+++ dist/scripts/app_main.js
@@ -289,7 +289,7 @@ function openView(status, filter, repo, target, formName) {
     myDataTable.render();
     myDataTable.dt.buttons().container().hide();
 
-    toggleView("view_pane");
+    toggleView(target);
 
     return myDataTable;
 }
@@ -1112,7 +1112,7 @@ function updateAttachmentStatus(DZ, bin_id, repo, status, process) {
             $("#maincontent :input, .dz-hidden-input").prop("disabled", false);
             return false;
         });
-    } 
+    }
 }
 
 /**
@@ -1371,6 +1371,60 @@ function addNavBarMenuItem(icon_class, button_class, title, action){
  * @param title {string}
  * @param action {function}  - function to be executed on click
  */
+//http://plnkr.co/edit/jhYu5iwoTSdhUDELvDIb?p=preview
+function JSONToCSVConvertor(JSONData,title, ShowLabel) {
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var CSV = '';
+    if (ShowLabel) {
+        var row = "";
+        for (var index in arrData[0]) {
+            row += index + ',';
+        }
+        row = row.slice(0, -1);
+        CSV += row + '\r\n';
+    }
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        for (var index in arrData[i]) {
+            var arrValue = arrData[i][index] == null ? "" : '="' + arrData[i][index] + '"';
+            row += arrValue + ',';
+        }
+        row.slice(0, row.length - 1);
+        CSV += row + '\r\n';
+    }
+    if (CSV == '') {
+        growl.error("Invalid data");
+        return;
+    }
+    var fileName = title;
+    if (msieversion()) {
+        var IEwindow = window.open();
+        IEwindow.document.write('sep=,\r\n' + CSV);
+        IEwindow.document.close();
+        IEwindow.document.execCommand('SaveAs', true, fileName + ".csv");
+        IEwindow.close();
+    } else {
+        var uri = 'data:application/csv;charset=utf-8,' + escape(CSV);
+        var link = document.createElement("a");
+        link.href = uri;
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+}
+function msieversion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    if (msie != -1 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number
+    {
+        return true;
+    } else { // If another browser,
+        return false;
+    }
+
+}
 function test(){
     let top = addNavBarItem("","","","","Top",null, "dropdown");
     let sub = addNavBarSubItem("","","","","Sub",null, "dropdown");
